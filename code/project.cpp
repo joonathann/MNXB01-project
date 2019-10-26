fb232821686e0acae96729a499dbabe15e004246
JonathanPet <jo4450pe-s@student.lu.se>
2019-10-26 18:40:15 +0200

    Another small update

--- code/project.cpp
+++ code/project.cpp
@@ -8,16 +8,18 @@
 
 void project() {
 	
-	string pathToFile = "/home/courseuser/MNXB01-project/datasets/smhi-opendata_Lund.csv"; 
-	//cout << "Please enter the full path to the file."; 
-	//cout << " It should look somehting like this: .../MNXB01-project/datasets/smhi-opendata_Lund.csv" << endl;
-	//cout << "Full path: ";
-	//cin >> pathToFile; //Put the path to your data file here
-	//cout << endl;
+	//string pathToFile = "/home/courseuser/MNXB01-project/datasets/smhi-opendata_Lund.csv"; 
+	string pathToFile;
+	cout << endl << "Please enter the full path to the file."; 
+	cout << " It should look somehting like this: .../MNXB01-project/datasets/smhi-opendata_Lund.csv" << endl;
+	cout << endl << "Full path: ";
+	cin >> pathToFile; //Put the path to your data file here
 	tempTrender t(pathToFile); //Instantiate your analysis object
 	
+	cout << endl << endl << "tempOnDay:" << endl << endl;
 	t.tempOnDay(12, 24, -2);
 	
-	t.MeanTempAndTempOnDate(1998, 1, 1);
+	cout << endl << endl << "MeanTempAndTempOnDate:" << endl << endl;
+	t.MeanTempAndTempOnDate(1977, 7, 7);
 	
 }

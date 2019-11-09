ade2979ec95455680f7f602b8f2587553dcbb9bb
Jonathan Widov <jonathanwidov1@gmail.com>
2019-11-09 17:36:12 +0100

    added function to calculate the yearly average for all years

---
+++ code/tempTrenderJW_cpp.d
@@ -0,0 +1,58 @@
+
+# DO NOT DELETE
+
+./tempTrenderJW_cpp.so: /usr/include/root/TH1.h /usr/include/root/TAxis.h
+./tempTrenderJW_cpp.so: /usr/include/root/TNamed.h
+./tempTrenderJW_cpp.so: /usr/include/root/TObject.h
+./tempTrenderJW_cpp.so: /usr/include/root/Rtypes.h
+./tempTrenderJW_cpp.so: /usr/include/root/RConfig.h
+./tempTrenderJW_cpp.so: /usr/include/root/RVersion.h
+./tempTrenderJW_cpp.so: /usr/include/root/DllImport.h
+./tempTrenderJW_cpp.so: /usr/include/root/Rtypeinfo.h
+./tempTrenderJW_cpp.so: /usr/include/root/snprintf.h
+./tempTrenderJW_cpp.so: /usr/include/root/strlcpy.h
+./tempTrenderJW_cpp.so: /usr/include/root/TGenericClassInfo.h
+./tempTrenderJW_cpp.so: /usr/include/root/TSchemaHelper.h
+./tempTrenderJW_cpp.so: /usr/include/root/TStorage.h
+./tempTrenderJW_cpp.so: /usr/include/root/TVersionCheck.h
+./tempTrenderJW_cpp.so: /usr/include/root/Riosfwd.h
+./tempTrenderJW_cpp.so: /usr/include/root/TBuffer.h
+./tempTrenderJW_cpp.so: /usr/include/root/TString.h
+./tempTrenderJW_cpp.so: /usr/include/root/TMathBase.h
+./tempTrenderJW_cpp.so: /usr/include/root/TAttAxis.h
+./tempTrenderJW_cpp.so: /usr/include/root/TArrayD.h
+./tempTrenderJW_cpp.so: /usr/include/root/TArray.h
+./tempTrenderJW_cpp.so: /usr/include/root/TAttLine.h
+./tempTrenderJW_cpp.so: /usr/include/root/TAttFill.h
+./tempTrenderJW_cpp.so: /usr/include/root/TAttMarker.h
+./tempTrenderJW_cpp.so: /usr/include/root/TArrayC.h
+./tempTrenderJW_cpp.so: /usr/include/root/TArrayS.h
+./tempTrenderJW_cpp.so: /usr/include/root/TArrayI.h
+./tempTrenderJW_cpp.so: /usr/include/root/TArrayF.h
+./tempTrenderJW_cpp.so: /usr/include/root/Foption.h
+./tempTrenderJW_cpp.so: /usr/include/root/TVectorFfwd.h
+./tempTrenderJW_cpp.so: /usr/include/root/TVectorDfwd.h
+./tempTrenderJW_cpp.so: /usr/include/root/TFitResultPtr.h
+./tempTrenderJW_cpp.so: /usr/include/root/TF1.h /usr/include/root/TFormula.h
+./tempTrenderJW_cpp.so: /usr/include/root/TBits.h
+./tempTrenderJW_cpp.so: /usr/include/root/TObjArray.h
+./tempTrenderJW_cpp.so: /usr/include/root/TSeqCollection.h
+./tempTrenderJW_cpp.so: /usr/include/root/TCollection.h
+./tempTrenderJW_cpp.so: /usr/include/root/TIterator.h
+./tempTrenderJW_cpp.so: /usr/include/root/Math/ParamFunctor.h
+./tempTrenderJW_cpp.so: /usr/include/root/TCanvas.h /usr/include/root/TPad.h
+./tempTrenderJW_cpp.so: /usr/include/root/TVirtualPad.h
+./tempTrenderJW_cpp.so: /usr/include/root/TAttPad.h
+./tempTrenderJW_cpp.so: /usr/include/root/TVirtualX.h
+./tempTrenderJW_cpp.so: /usr/include/root/TAttText.h
+./tempTrenderJW_cpp.so: /usr/include/root/GuiTypes.h
+./tempTrenderJW_cpp.so: /usr/include/root/Buttons.h
+./tempTrenderJW_cpp.so: /usr/include/root/TQObject.h
+./tempTrenderJW_cpp.so: /usr/include/root/TAttBBox2D.h
+./tempTrenderJW_cpp.so: /usr/include/root/TPoint.h
+./tempTrenderJW_cpp.so: /usr/include/root/TAttCanvas.h
+./tempTrenderJW_cpp.so: /usr/include/root/TCanvasImp.h
+./tempTrenderJW_cpp.so: /usr/include/root/TLegend.h /usr/include/root/TPave.h
+./tempTrenderJW_cpp.so: /usr/include/root/TBox.h tempTrender.h
+./tempTrenderJW_cpp.so: /usr/include/root/cintdictversion.h /usr/include/root/RVersion.h
+tempTrenderJW_cpp__ROOTBUILDVERSION= 5.34/30
